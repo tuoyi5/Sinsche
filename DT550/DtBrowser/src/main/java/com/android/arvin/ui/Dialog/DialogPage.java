5ef605ac3779867d42dcbfe28100c2bd9734e5c0
sujinlin <sujinlin@onyx-international.com>
2017-09-09 19:05:52 +0800

    新增dialog布局相关

---
+++ DT550/DtBrowser/src/main/java/com/android/arvin/ui/Dialog/DialogPage.java
@@ -0,0 +1,74 @@
+package com.android.arvin.ui.Dialog;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.android.arvin.R;
+
+/**
+ * Created by arvin on 2017/9/9 0009.
+ */
+
+public class DialogPage extends RelativeLayout {
+
+    private TextView ValueTitleText, ValueText;
+    private ImageView valueImage;
+
+    public DialogPage(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    private void init(Context context) {
+        View.inflate(context, R.layout.dialog_page_item, DialogPage.this);
+        ValueTitleText = (TextView) findViewById(R.id.value_title_text);
+        ValueText = (TextView) findViewById(R.id.value_text);
+        valueImage = (ImageView) findViewById(R.id.value_image);
+    }
+
+    public void setValueTitleText(String string) {
+        ValueTitleText.setText(string);
+    }
+
+    public void setValueText(String string) {
+        ValueText.setText(string);
+    }
+
+    public void setValueImage(int id) {
+        this.valueImage.setImageResource(id);
+    }
+
+    class DialogPageData{
+        String valueTitle;
+        String value;
+        int valueImage;
+
+        public String getValueTitle() {
+            return valueTitle;
+        }
+
+        public void setValueTitle(String valueTitle) {
+            this.valueTitle = valueTitle;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+
+        public int getValueImage() {
+            return valueImage;
+        }
+
+        public void setValueImage(int valueImage) {
+            this.valueImage = valueImage;
+        }
+    }
+}
